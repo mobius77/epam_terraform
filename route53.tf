2608f806b0e8c11d871f36998eb8a8cea4a41131
mobius77 <admin@musienko.zp.ua>
2023-02-04 19:32:33 +0200

    record

--- route53.tf
+++ route53.tf
@@ -2,20 +2,24 @@ resource "aws_route53_record" "subdomain-for-test" {
   zone_id = "Z01002213NIE3APYTM5XX"
   name    = "test.mobiusz.tk"
   type    = "A"
-  alias {
-    name                   = aws_instance.my_project_instance.public_dns
-    zone_id               = "Z01002213NIE3APYTM5XX"
-    evaluate_target_health = true
-  }
+  ttl     = 300
+  records = [aws_instance.my_project_instance.public_ip]
+  # alias {
+  #   name                   = aws_instance.my_project_instance.public_dns
+  #   zone_id               = "Z01002213NIE3APYTM5XX"
+  #   evaluate_target_health = true
+  # }
 }
 
 resource "aws_route53_record" "subdomain-for-prod" {
   zone_id = "Z01002213NIE3APYTM5XX"
   name    = "prod.mobiusz.tk"
   type    = "A"
-  alias {
-    name                   = aws_instance.my_project_instance_prod.public_dns
-    zone_id               = "Z01002213NIE3APYTM5XX"
-    evaluate_target_health = true
-  }
+  ttl     = 300
+  records = [aws_instance.my_project_instance_prod.public_ip]
+  # alias {
+  #   name                   = aws_instance.my_project_instance_prod.public_dns
+  #   zone_id               = "Z01002213NIE3APYTM5XX"
+  #   evaluate_target_health = true
+  # }
 }
