86d6d9929a54b3ac4b4e74abe870596fd948f3a3
mobius77 <admin@musienko.zp.ua>
2023-02-12 14:56:22 +0200

    prod instace ip

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "instance_ip" {
+output "test_instance_ip" {
   value = aws_instance.my_project_instance.public_ip
+}
+
+output "prod_instance_ip" {
+  value = aws_instance.my_project_instance_prod.public_ip
 }
